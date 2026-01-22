f5c6e7f1370358575602524fdd99e2b5090b9de2
caio-vasconcellos <caiopvasconcellos@gmail.com>
2026-01-22 11:42:43 -0300

    Add files via upload

---
+++ aula7/analises_projetofinal.sql
@@ -0,0 +1,36 @@
+USE Empresa
+
+SELECT * FROM Categorias
+SELECT * FROM Clientes
+SELECT * FROM DetalhesPed
+SELECT * FROM Fornecedores
+SELECT * FROM Funcionarios
+SELECT * FROM Pedidos
+SELECT * FROM Produtos
+
+/* ------------------------------------ */
+-- LINK DO GITHUB COM MATERIAL DAS AULAS: https://github.com/Victorlcr/SQL-Essential-1S-25
+/* ------------------------------------ */
+
+/* ------------------------------------
+-- COLOQUE ABAIXO O NOME COMPLETO DOS INTEGRANTES DO GRUPO, FAVOR SALVAR O ARQUIVO COMO sql_projeto_nome de um integrante do grupo
+
+
+
+------------------------------------ */
+
+
+-- 1 - Quantidade de faturamento geral
+
+-- 2 - Quantidade de pedidos geral
+
+-- 3 - Quantidade de clientes geral
+
+-- 4 - Densidade de pedidos por Pais (Quantidade de Pedidos)
+
+-- 5 - Nome do Funcionario, faturamento gerado pelo funcionário e quantidade de pedidos atendidos por ele
+
+-- 6 - Consulta opcional, usem a curiosidade e criatividade de vocês!
+
+
+
